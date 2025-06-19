3f3de4a41d9e9631f7408a3f5991f4f7ea96838c
poluektovaalina <poluektova2315@gmail.com>
2025-06-19 19:13:18 +0500

    front

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,42 +1,85 @@
-import { useState } from 'react'
-import logoOlimpic from '../../images/logo-white.png'
-import banner from '../../images/frame.png'
-import icoCountries from '../../images/ico-countries.svg'
-import icoDisciplines from '../../images/ico-disciplines.svg'
+import { useState } from "react";
+import '../components/Countries.css'
+import logoOlimpic from "../../images/logo-white.png";
+import icoPrev from "../../images/ico-prev.svg";
+import icoUSA from "../../images/flags/USA.png";
+import icoChina from "../../images/flags/China.png";
+import icoJapan from "../../images/flags/Japan.png";
+import icoGermany from "../../images/flags/Germany.png";
+import icoAustralia from "../../images/flags/Australia.png";
+import icoRussia from "../../images/flags/Russia.png";
+import icoUnitedKingdom from "../../images/flags/UnitedKingdom.png";
+import icoFrance from "../../images/flags/France.png";
 export function Countries() {
-
   return (
     <>
-     <div className="container">
-      <div className="main">
-        <div className="logo">
-          <img src={logoOlimpic} alt="" />
-        </div>
-        <div className="banner">
-          <img src={banner} alt="" />
-        </div>
-        <div className="btn_group">
-          <button className='btn countries'>
-            <div className="lolo_btn">
-              <img src={icoCountries} alt="ico-countries" />
+      <div className="container">
+        <div className="countries">
+          <div className="header">
+            <div className="prev_btn">
+              <img src={icoPrev} alt="" />
             </div>
-            <span className="title_btn">
-              Countries
-            </span>
-          </button>
-          <button className='btn disciplines'>
-            <div className="lolo_btn">
-              <img src={icoDisciplines} alt="ico-countries" />
+            <div className="logo">
+              <img src={logoOlimpic} alt="" />
             </div>
-            <span className="title_btn">
-              Disciplines
-            </span>
-          </button>
+          </div>
+          <div className="title">
+            <span>Countries</span>
+          </div>
+          <div className="group_countries">
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoUSA} alt="ico-countries" />
+              </div>
+              <span className="title_btn">USA</span>
+            </button>
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoChina} alt="ico-countries" />
+              </div>
+              <span className="title_btn">China</span>
+            </button>
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoJapan} alt="ico-countries" />
+              </div>
+              <span className="title_btn">Japan</span>
+            </button>
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoGermany} alt="ico-countries" />
+              </div>
+              <span className="title_btn">Germany</span>
+            </button>
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoAustralia} alt="ico-countries" />
+              </div>
+              <span className="title_btn">Australia</span>
+            </button>
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoRussia} alt="ico-countries" />
+              </div>
+              <span className="title_btn">Russia</span>
+            </button>
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoUnitedKingdom} alt="ico-countries" />
+              </div>
+              <span className="title_btn">United Kingdom</span>
+            </button>
+            <button className="btn">
+              <div className="lolo_btn">
+                <img src={icoFrance} alt="ico-countries" />
+              </div>
+              <span className="title_btn">France</span>
+            </button>
+          </div>
         </div>
       </div>
-     </div>
     </>
-  )
+  );
 }
 
-export default Countries
+export default Countries;
